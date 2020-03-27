3e353a12b2849ce639da3065e34048f6b53ea337
Ola Tomczyk <ola443@op.pl>
2020-03-27 17:14:53 +0100

    buttons func changed

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const guitaristImg = document.querySelector('.guitarist-img')
 let blackWhiteButton = document.querySelector('.black-white')
 
 let blackWhiteImg = event => {
-    if (guitaristImg.style.filter !== 'greyscale(100%)') { guitaristImg.style.filter = 'greyscale(100%)' }
+    if (guitaristImg.style.filter !== 'grayscale(100%)') { guitaristImg.style.filter = 'grayscale(100%)' }
     else { guitaristImg.style.filter = 'none' }
 }
 
 blackWhiteButton.addEventListener('click', blackWhiteImg)
 
 let borderRadiusButton = document.querySelector('.border-radius')
-let borderRadiusChange = event => guitaristImg.style.borderRadius = '50%';
+let borderRadiusChange = event => {
+    if (guitaristImg.style.borderRadius !== '50%') { guitaristImg.style.borderRadius = '50%' }
+    else { guitaristImg.style.borderRadius = '0%' }
+};
 borderRadiusButton.addEventListener('click', borderRadiusChange)
 
 let opacityButton = document.querySelector('.opacity')
-let imgOpacity = event => guitaristImg.style.opacity = '0.5';
+let imgOpacity = event => {
+    if (guitaristImg.style.opacity !== '0.5') { guitaristImg.style.opacity = '0.5' }
+    else { guitaristImg.style.opacity = '1' }
+};
 opacityButton.addEventListener('click', imgOpacity)
 
