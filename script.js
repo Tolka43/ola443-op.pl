281f7f68350d01d2f25a482c723024df38fbe7b5
Ola Tomczyk <ola443@op.pl>
2020-04-01 14:27:50 +0200

    changes

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-const guitaristImg = document.querySelector('.guitarist-img')
-
-let blackWhiteButton = document.querySelector('.black-white')
-
-let blackWhiteImg = event => {
-    if (guitaristImg.style.filter !== 'grayscale(100%)') { guitaristImg.style.filter = 'grayscale(100%)' }
-    else { guitaristImg.style.filter = 'none' }
-}
-
-blackWhiteButton.addEventListener('click', blackWhiteImg)
-
-let borderRadiusButton = document.querySelector('.border-radius')
-let borderRadiusChange = event => {
-    if (guitaristImg.style.borderRadius !== '50%') { guitaristImg.style.borderRadius = '50%' }
-    else { guitaristImg.style.borderRadius = '0%' }
-};
-borderRadiusButton.addEventListener('click', borderRadiusChange)
-
-let opacityButton = document.querySelector('.opacity')
-let imgOpacity = event => {
-    if (guitaristImg.style.opacity !== '0.5') { guitaristImg.style.opacity = '0.5' }
-    else { guitaristImg.style.opacity = '1' }
-};
-opacityButton.addEventListener('click', imgOpacity)
-
-let resetButton = document.querySelector('.reset')
-let resetFunc = event => {guitaristImg.style.removeProperty('filter'); guitaristImg.style.removeProperty('opacity'); guitaristImg.style.removeProperty('border-radius')};
-resetButton.addEventListener('click', resetFunc)
-
 const inputFile = document.querySelector('input[type=file]');
 
 function drawImage(file) {
-    const container = document.querySelector('.first-image');
+  const container = document.querySelector('.first-image');
 
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
@@ -38,7 +9,7 @@ function drawImage(file) {
   const img = new Image();
   img.src = URL.createObjectURL(file);
 
-  img.addEventListener('load', function() {
+  img.addEventListener('load', function () {
     const ourWidth = 250;
     const ourHeight = (img.height / img.width) * 250;
 
@@ -51,6 +22,39 @@ function drawImage(file) {
   });
 }
 
-inputFile.addEventListener('change', function(event) {
+inputFile.addEventListener('change', function (event) {
   drawImage(inputFile.files[0]);
 });
+
+let blackWhiteButton = document.querySelector('.black-white')
+
+let blackWhiteImg = event => {
+  const canvas = document.querySelector('canvas')
+  if (canvas.style.filter !== 'grayscale(100%)') { canvas.style.filter = 'grayscale(100%)' }
+  else { canvas.style.filter = 'none' }
+}
+
+blackWhiteButton.addEventListener('click', blackWhiteImg)
+
+let borderRadiusButton = document.querySelector('.border-radius')
+let borderRadiusChange = event => {
+  const canvas = document.querySelector('canvas')
+  if (canvas.style.borderRadius !== '50%') { canvas.style.borderRadius = '50%' }
+  else { canvas.style.borderRadius = '0%' }
+};
+borderRadiusButton.addEventListener('click', borderRadiusChange)
+
+let opacityButton = document.querySelector('.opacity')
+let imgOpacity = event => {
+  const canvas = document.querySelector('canvas')
+  if (canvas.style.opacity !== '0.5') { canvas.style.opacity = '0.5' }
+  else { canvas.style.opacity = '1' }
+};
+opacityButton.addEventListener('click', imgOpacity)
+
+let resetButton = document.querySelector('.reset')
+let resetFunc = event => {
+  const canvas = document.querySelector('canvas')
+   canvas.style.removeProperty('filter'); canvas.style.removeProperty('opacity'); canvas.style.removeProperty('border-radius')
+   };
+resetButton.addEventListener('click', resetFunc)
